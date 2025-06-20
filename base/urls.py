8b7773be2f830cf8e62876c4817b8e1ef463b439
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-06-20 22:41:26 +0500

    djsfhnadejkw

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("detail/<int:detail_id>/", detail_clothes, name="detail"),
     path('filter/<str:jinsi>/', kiyimlar_view, name='jins_filter'),
     path('skidkalar/', skidkali_kiyimlar_view, name='skidkali_kiyimlar'),
+    path("yosh/<str:yosh>/", yosh_boyicha, name="yosh_boyicha"),
 ]
--- base/views.py
+++ base/views.py
@@ -27,6 +27,16 @@ def skidkali_kiyimlar_view(request):
     kiyimlar = Main_Kiyim.objects.filter(skidkasi__isnull=False)
     return render(request, 'skidka_kiyimlar.html', {'kiyimlar': kiyimlar})
 
-def yosh_boyicha_kiyimlar(request, yosh):
-    kiyimlar = Main_Kiyim.objects.filter(yoshi=yosh)
-    return render(request, "kiyimlar.html", {"kiyimlar": kiyimlar, "yosh": yosh})
+def yosh_boyicha(request, yosh):
+    yoshlar = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    yosh = request.GET.get('yosh')
+
+    if yosh:
+        kiyimlar = Main_Kiyim.objects.filter(yoshi=yosh)
+    else:
+        kiyimlar = Main_Kiyim.objects.all()
+
+    return render(request, 'index.html', {
+        'kiyimlar': kiyimlar,
+        'yoshlar': yoshlar,
+    })
